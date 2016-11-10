d986c690757a168c3dc17727de15b842c851951e
#云淡然 <cloudcome@163.com>
2016-11-10 17:13:42 +0800

    ~ 样式

--- example/index.js
+++ example/index.js
@@ -2,22 +2,29 @@
 
 
 var Dialog = require('../src/index');
+var Loading = require('blear.ui.loading');
 
 var dialog = new Dialog({
-    el: '#demo'
+    el: '#demo',
+    width: 400,
+    height: 300,
+    maxHeight: 300
 });
-
+var loading = new Loading();
 
 document.getElementById('open').onclick = function () {
     dialog.open();
 };
 
-
 document.getElementById('resize').onclick = function () {
     dialog.resize();
 };
 
-
 document.getElementById('close').onclick = function () {
     dialog.close();
+};
+
+document.getElementById('open2').onclick = function () {
+    loading.open();
+    dialog.open();
 };
