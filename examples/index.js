2fdba7a5679c346dc490d74ed641c33c069dd01c
#云淡然 <cloudcome@163.com>
2018-10-12 13:47:52 +0800

    ~ update module

--- examples/index.js
+++ examples/index.js
@@ -5,25 +5,11 @@ var Dialog = require('../src/index');
 
 var dialog = new Dialog({
     el: '#demo',
-    width: 400,
-    maskHit: true,
-    maskOptions: {
-        opacity: 0.9
-    }
+    width: 400
 });
 
 document.getElementById('open').onclick = function () {
     dialog.open();
 };
 
-document.getElementById('resize').onclick = function () {
-    dialog.resize();
-};
-
-document.getElementById('close').onclick = function () {
-    dialog.close();
-};
-
-document.getElementById('open2').onclick = function () {
-    dialog.open();
-};
+window.dialog = dialog;
